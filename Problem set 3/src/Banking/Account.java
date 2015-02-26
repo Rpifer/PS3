a01ec509070902877ffacc2b5e7ed5c7d7c73c87
Rpifer <Rpifer@udel.edu>
2015-02-26 18:54:46 -0500

    first commit/push

---
+++ Problem set 3/src/Banking/Account.java
@@ -0,0 +1,19 @@
+package Banking;
+
+import java.util.Date;
+
+public class Account {
+	
+	private int id;
+	private double balance;
+	private double annualInterestRate;
+	private Date dateCreated;
+	
+	public Account(){
+		this.id = 0;
+		this.balance = 0;
+		this.annualInterestRate = 0;
+		this.dateCreated = new Date();
+		
+	}
+}
